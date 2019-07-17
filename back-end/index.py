d643357ba7278b441530a6344b7e8d66c95e52b5
Joshua Ho <ho.joshua4@gmail.com>
2019-07-16 19:29:02 -0700

    Completed location fetch via Flask

--- back-end/index.py
+++ back-end/index.py
@@ -1,94 +1,119 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from urllib.parse import urlparse, parse_qs
+# from http.server import BaseHTTPRequestHandler, HTTPServer
+# from urllib.parse import urlparse, parse_qs
 
-import os
-import json
+#.env attributes
+
+# import os
+# import json
+
 import requests
+from flask import Flask, jsonify, request
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from models.location import Location
 
 
-PORT = 3000
+app = Flask(__name__)
 
-#Build handler for HTTP Get Request
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+#ROUTES TO BUILD
+#WEATHER
+@app.route('/weather')
+def weather():
+  # http://localhost:5000/weather?data[latitude]=41.3850639&data[longitude]=2.1734035
+  return 'Weather page'
+#LOCATION
+@app.route('/location')
+def location():
+  city = request.args.get('city')
+  return Location.fetch(city)
+#EVENTS
+@app.route('/events')
+def events():
+  return 'Events page'
 
-  def do_GET(self):
+# #Build handler for HTTP Get Request
+# class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
-    ##Detects url path
-    parsed_path = urlparse(self.path)
-    print('request path', parsed_path.path)
+#   def do_GET(self):
 
-    ##parses query
-    parsed_qs = parse_qs(parsed_path.query)
-    print('parsed query', parsed_qs)
+#     ##Detects url path
+#     parsed_path = urlparse(self.path)
+#     print('request path', parsed_path.path)
 
-    if parsed_path.path == '/locations':
+#     ##parses query
+#     parsed_qs = parse_qs(parsed_path.query)
+#     print('parsed query', parsed_qs)
+
+#     if parsed_path.path == '/locations':
       
-      #Basic HTTP request info
-      self.send_response(200)      
-      self.send_header('Content-type', 'application/json')
-      self.end_headers()
+#       #Basic HTTP request info
+#       self.send_response(200)      
+#       self.send_header('Content-type', 'application/json')
+#       self.end_headers()
 
-      query = parsed_qs['place']
-      api_key = 'AIzaSyCz4_13_FImCJdgbqeWYrglhmkcBRW5mgg'    
+#       query = parsed_qs['place']
+#       api_key = 'AIzaSyCz4_13_FImCJdgbqeWYrglhmkcBRW5mgg'    
 
-      # http://localhost:3000/weather?data[latitude]=41.3850639&data[longitude]=2.1734035
+#       # http://localhost:3000/weather?data[latitude]=41.3850639&data[longitude]=2.1734035
 
-      #Construct our query
-      url = f'https://maps.googleapis.com/maps/api/geocode/json?address={query}&key={api_key}'
+#       #Construct our query
+#       url = f'https://maps.googleapis.com/maps/api/geocode/json?address={query}&key={api_key}'
 
-      # print('url', url)
+#       # print('url', url)
 
-      #Parse incoming data string into python or json
-      result = requests.get(url).json()
-      print('results!!!!', result['results'][0]['geometry']['location'])
+#       #Parse incoming data string into python or json
+#       result = requests.get(url).json()
+#       print('results!!!!', result['results'][0]['geometry']['location'])
       
 
 
-      ##Create a new class and push it to the page
+#       ##Create a new class and push it to the page
       
-      #Print JSON onto page using wfile.write
-      json_string = json.dumps(result)
-      self.wfile.write(json_string.encode())
+#       #Print JSON onto page using wfile.write
+#       json_string = json.dumps(result)
+#       self.wfile.write(json_string.encode())
       
-      # { 'formatted_query':'the place', 
-      # 'search_query':'what they typed', 
-      # 'latitude':100.0, 
-      # 'longitude':100.0 }
+#       # { 'formatted_query':'the place', 
+#       # 'search_query':'what they typed', 
+#       # 'latitude':100.0, 
+#       # 'longitude':100.0 }
 
-      return
+#       return
 
-def create_server():
-  return HTTPServer(
-    ('127.0.0.1', PORT), SimpleHTTPRequestHandler
-  )
+# def create_server():
+#   return HTTPServer(
+#     ('127.0.0.1', PORT), SimpleHTTPRequestHandler
+#   )
 
-def run_forever():
-  server = create_server()
+# def run_forever():
+#   server = create_server()
 
-  try:
-    print(f'Starting server on port {PORT}')
-    server.serve_forever()
-  except KeyboardInterrupt:
-    server.server_close()
-    server.shutdown()
-    print('Server Closed')
+#   try:
+#     print(f'Starting server on port {PORT}')
+#     server.serve_forever()
+#   except KeyboardInterrupt:
+#     server.server_close()
+#     server.shutdown()
+#     print('Server Closed')
 
-if __name__ == "__main__":
-    run_forever()
-    handler = SimpleHTTPRequestHandler()
+# if __name__ == "__main__":
+#     run_forever()
+#     handler = SimpleHTTPRequestHandler()
 
-# { 'formatted_query':'the place', 
-# 'search_query':'what they typed', 
-# 'latitude':100.0, 
-# 'longitude':100.0 }
+# # { 'formatted_query':'the place', 
+# # 'search_query':'what they typed', 
+# # 'latitude':100.0, 
+# # 'longitude':100.0 }
 
-class LocationQuery:
+# class LocationQuery:
 
-  def __init__(self,query,search,lat,long):
-    self.formatted_query = query
-    self.search_query = search
-    self.latitude = lat
-    self.longitude = long
+#   def __init__(self,query,search,lat,long):
+#     self.formatted_query = query
+#     self.search_query = search
+#     self.latitude = lat
+#     self.longitude = long
 
-  def serialize(self):
-    return vars(self)
+#   def serialize(self):
+#     return vars(self)
